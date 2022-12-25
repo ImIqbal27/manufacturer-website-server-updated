d3ce2405e9c0cdb9142f0ae56b8aa010d91c6e3f
unknown <iamhasan9501@gmail.com>
2022-12-25 15:10:04 +0600

    hjgh

--- index.js
+++ index.js
@@ -14,20 +14,20 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 //////////////////////////// JWT ///////////////////////
 
-// function verifyJWT(req, res, next) {
-//     const authHeader = req.headers.authorization;
-//     if (!authHeader) {
-//         return res.status(401).send({ message: 'UnAuthorized access' });
-//     }
-//     const token = authHeader.split(' ')[1];
-//     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-//         if (err) {
-//             return res.status(403).send({ message: 'Forbidden access' })
-//         }
-//         req.decoded = decoded;
-//         next();
-//     });
-// }
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'UnAuthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    });
+}
 ////////////////////////////////////////////////////////////////////////
 async function run() {
     try {
@@ -82,7 +82,7 @@ async function run() {
                 $set: user,
             };
             const result = await userCollection.updateOne(filter, updateDoc, options);
-            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1000h' })
+            const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '999999999999999h' })
             res.send({ result, token });
         });
         /////////////////// put admin user  //////////////////////////////////////////////////////////
@@ -109,7 +109,7 @@ async function run() {
                 res.send(result);
             }
             else {
-                res.status(403).send({ message: 'Forbidden' });
+                res.status(403).send({ message: 'Forbidden -rumon' });
             }
         });
         /////////////////////////// admin ////////////////////////////
